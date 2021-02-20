b24048b2938f55c1b48429fa5d4bc183dbbf5c79
heniojunior <henioajunior@gmail.com>
2021-02-19 21:38:03 -0300

    Criando o endpoint /produtos/{id}

---
+++ src/main/java/com/crystaldata/sistemapedido/resources/ProdutoResource.java
@@ -0,0 +1,25 @@
+package com.crystaldata.sistemapedido.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crystaldata.sistemapedido.domain.Produto;
+import com.crystaldata.sistemapedido.services.ProdutoService;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoResource {
+
+	@Autowired
+	private ProdutoService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Produto obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}

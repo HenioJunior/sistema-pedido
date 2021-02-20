4397aa4927243b5922997bb674f7f836b3eca282
heniojunior <henioajunior@gmail.com>
2021-02-19 21:33:38 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/crystaldata/sistemapedido/domain/Categoria.java
+++ src/main/java/com/crystaldata/sistemapedido/domain/Categoria.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -19,6 +21,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
@@ -51,9 +54,9 @@ public class Categoria implements Serializable {
 		return produtos;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
+	/*public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
-	}
+	}*/
 
 	@Override
 	public int hashCode() {

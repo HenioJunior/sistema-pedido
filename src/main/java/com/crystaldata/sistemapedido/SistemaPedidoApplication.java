f6d1ae003d7d784571d252026c8fdac9f40957d2
Henio Junior <henioajunior@gmail.com>
2021-02-22 16:59:40 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/crystaldata/sistemapedido/SistemaPedidoApplication.java
+++ src/main/java/com/crystaldata/sistemapedido/SistemaPedidoApplication.java
@@ -13,6 +13,7 @@ import com.crystaldata.sistemapedido.entities.Cidade;
 import com.crystaldata.sistemapedido.entities.Cliente;
 import com.crystaldata.sistemapedido.entities.Endereco;
 import com.crystaldata.sistemapedido.entities.Estado;
+import com.crystaldata.sistemapedido.entities.ItemPedido;
 import com.crystaldata.sistemapedido.entities.Pagamento;
 import com.crystaldata.sistemapedido.entities.PagamentoComBoleto;
 import com.crystaldata.sistemapedido.entities.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.crystaldata.sistemapedido.repositories.CidadeRepository;
 import com.crystaldata.sistemapedido.repositories.ClienteRepository;
 import com.crystaldata.sistemapedido.repositories.EnderecoRepository;
 import com.crystaldata.sistemapedido.repositories.EstadoRepository;
+import com.crystaldata.sistemapedido.repositories.ItemPedidoRepository;
 import com.crystaldata.sistemapedido.repositories.PagamentoRepository;
 import com.crystaldata.sistemapedido.repositories.PedidoRepository;
 import com.crystaldata.sistemapedido.repositories.ProdutoRepository;
@@ -34,38 +36,41 @@ public class SistemaPedidoApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPedidoApplication.class, args);
 	}
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -79,7 +84,7 @@ public class SistemaPedidoApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 
@@ -92,7 +97,7 @@ public class SistemaPedidoApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
@@ -104,7 +109,7 @@ public class SistemaPedidoApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
@@ -113,13 +118,27 @@ public class SistemaPedidoApplication implements CommandLineRunner {
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
 
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
 		ped2.setPagamento(pagto2);
 
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 }
--- src/main/java/com/crystaldata/sistemapedido/entities/Produto.java
+++ src/main/java/com/crystaldata/sistemapedido/entities/Produto.java
@@ -2,7 +2,9 @@ package com.crystaldata.sistemapedido.entities;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,11 +13,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-public class Produto  implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -31,6 +34,9 @@ public class Produto  implements Serializable {
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
 	)
 	private List<Categoria> categorias = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Produto() {
 	}
@@ -40,6 +46,15 @@ public class Produto  implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getpedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -70,9 +85,17 @@ public class Produto  implements Serializable {
 		return categorias;
 	}
 
-	/*public void setCategorias(List<Categoria> categorias) {
+	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
-	}*/
+	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
